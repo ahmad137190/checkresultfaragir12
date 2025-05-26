c48a7e085a1d28a63eb133a54e0f2220e6dd8f7f
ahmad <a.khalili137190@gmail.com>
2025-05-26 10:52:38 +0330

    add success image

--- a.py
+++ a.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 import time
 import subprocess
 import json
+from PIL import Image
+import os
 
 # رشته کوکی‌ها
-cookie_string = "_ga=GA1.1.177117572.1745664028; csrftoken=WMP2Xmc572WnOHOd0zFM1HM3prWkLucTPFt06pAaGMXEqqhUSbRoaxB3QkKkNeNQ; sessionid=s1itp6ksyycvo8ywiwn12jkpw1ywzfhh; __arcsjs=d51fee7ea059f9ec9fc2649a7f818d5c; _ga_VC3V6PM6FB=GS2.1.s1748231659$o50$g1$t1748232304$j0$l0$h0"
+cookie_string = "_ga=GA1.1.177117572.1745664028; csrftoken=WMP2Xmc572WnOHOd0zFM1HM3prWkLucTPFt06pAaGMXEqqhUSbRoaxB3QkKkNeNQ; __arcsjs=eb633c11f7f5c45abd14267dceb097e5; sessionid=h94i31lhw8xc0r0w88kqmfud6pv6r0d8; _ga_VC3V6PM6FB=GS2.1.s1748242011$o52$g1$t1748242036$j0$l0$h0"
 
 # تبدیل رشته به دیکشنری
 cookies = {item.split("=")[0].strip(): item.split("=")[1].strip() for item in cookie_string.split(";")}
@@ -54,6 +56,33 @@ def send_sms():
     except requests.exceptions.RequestException as e:
         print(f"⚠️ خطا در ارسال پیامک: {e}")
 
+def display_image_in_terminal(image_path):
+    try:
+        script_dir = os.path.dirname(os.path.abspath(__file__))  # مسیر فایل اسکریپت
+        image_path = os.path.join(script_dir, "success.png")
+
+        #success_image = PhotoImage(file=image_path)
+        # باز کردن تصویر
+        img = Image.open(image_path)
+
+        # تغییر اندازه برای ترمینال
+        img = img.resize((50, 25))  # عرض و ارتفاع تصویر در ترمینال
+
+        # تبدیل به متن ASCII
+        img = img.convert("L")  # تبدیل به حالت خاکستری
+        chars = "@%#*+=-:. "  # کاراکترهای ASCII برای شدت نور
+        ascii_str = ""
+        for pixel in img.getdata():
+            ascii_str += chars[pixel // 25]  # شدت نور به کاراکتر
+
+        # گروه‌بندی خطوط
+        ascii_str = "\n".join([ascii_str[i:(i + 50)] for i in range(0, len(ascii_str), 50)])
+
+        # نمایش تصویر
+        print(ascii_str)
+    except Exception as e:
+        print(f"⚠️ خطا در نمایش تصویر: {e}")
+
 was_disabled = True
 while True:
     try:
@@ -68,11 +97,11 @@ while True:
                 if was_disabled and not is_disabled:
                     print("✅ اعلام نتیجه نهایی فعال شد!")
                     send_sms()  # ارسال پیامک
+                    display_image_in_terminal("success.png")  # نمایش تصویر ASCII در ترمینال
                     subprocess.run(["open", "-a", "Music"])  # پخش موسیقی
                     break
                 else:
                     print("⏳ هنوز اعلام نتیجه نهایی غیرفعال است...")
-                    #send_sms()  # ارسال پیامک در حالت غیرفعال
                     was_disabled = is_disabled
             else:
                 print("⚠️ تگ <a> بالای متن 'اعلام نتیجه نهایی' پیدا نشد.")
